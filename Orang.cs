50013f0c4c87cb1335bc7268bbec82baf38d5994
jhonyhar <jhonyhar@gmail.com>
2015-03-12 10:39:03 +0700

    c1

---
+++ Orang.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFT1
+{
+  class Orang
+  {
+    public int Id { get; set; }
+    public string Nama { get; set; }
+    public DateTime TanggalLahir { get; set; }
+    private int _usia;
+
+    public int Usia
+    {
+      get { return DateTime.Today.Year - TanggalLahir.Year; }
+    }
+
+    public string Alamat { get; set; }
+  }
+}
